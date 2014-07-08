e8aa172d6d9adacb2f9a3b6129afbd0a21facf6e
Robert Christensen <rchristensen@wesleyan.edu>
2014-07-08 12:56:39 -0400

    Got file copying working fairly well
    Added README and comments as well..

---
+++ README.md
@@ -0,0 +1,3 @@
+# logcap
+
+A simple script to capture system, nginx and mysql logs.  Written as an exercise for GitHub Enterprise interview questions.
--- logcap.rb
+++ logcap.rb
@@ -1,6 +1,31 @@
+# We're using libraries included with nearly all Ruby installations
+# for maximum compatibility
 require 'fileutils'
 require 'rubygems/package'
 require 'zlib'
 
-FileUtils.mkdir_p %w( /tmp/logs/system-logs
-                      /tmp/logs/database-logs/ /tmp/logs/web-logs )
+# Define our temporary directories.
+sysdir = '/tmp/logs/system-logs'
+dbdir = '/tmp/logs/database-logs'
+webdir = '/tmp/logs/web-logs'
+
+# Create our directory structure to copy our logs to.
+FileUtils.mkdir_p %W(#{sysdir} #{dbdir} #{webdir})
+
+# Copy auth logs, using a wildcard so we include compressed rotated logs
+# to our system-logs directory.
+Dir.glob('/var/log/auth*') do
+  |f| FileUtils.cp File.expand_path(f), sysdir
+end
+
+# Copy dmesg logs, using a wildcard so we include compressed rotated logs
+# to our system-logs directory.
+Dir.glob('/var/log/dmesg*') do
+  |f| FileUtils.cp File.expand_path(f), sysdir
+end
+
+# Copy syslog logs, using a wildcard so we include compressed rotated logs
+# to our system-logs directory.
+Dir.glob('/var/log/syslog*') do
+  |f| FileUtils.cp File.expand_path(f), sysdir
+end

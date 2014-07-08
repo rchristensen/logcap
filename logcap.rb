28dfba0f2bb8e90761949c6f24e08cccc652cef2
Robert Christensen <rchristensen@wesleyan.edu>
2014-07-08 13:30:13 -0400

    Added nginx and mysql log copying
    Had to use recursive cp on mysql because they’re weird.

--- logcap.rb
+++ logcap.rb
@@ -18,14 +18,26 @@ Dir.glob('/var/log/auth*') do
   |f| FileUtils.cp File.expand_path(f), sysdir
 end
 
-# Copy dmesg logs, using a wildcard so we include compressed rotated logs
-# to our system-logs directory.
+# Copy dmesg logs
 Dir.glob('/var/log/dmesg*') do
   |f| FileUtils.cp File.expand_path(f), sysdir
 end
 
-# Copy syslog logs, using a wildcard so we include compressed rotated logs
-# to our system-logs directory.
+# Copy syslog logs
 Dir.glob('/var/log/syslog*') do
   |f| FileUtils.cp File.expand_path(f), sysdir
+end
+
+# Copy nginx logs, using a wildcard so we include error, access logs and
+# their compressed rotated friends to our web-logs directory.
+Dir.glob('/var/log/nginx/*') do
+  |f| FileUtils.cp File.expand_path(f), webdir
+end
+
+# Copy MySQL logs, using a wildcard so we include logs and
+# their compressed rotated friends to our database-logs directory.
+# Also using FileUtils.cp_r since MySQL is weird and has log files
+# both inside and outside it's log directory.
+Dir.glob('/var/log/mysql*') do
+  |f| FileUtils.cp_r File.expand_path(f), dbdir
 end
